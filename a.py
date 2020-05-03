3b2bfa66ad1928d02382adda069957def8325b5b
dngthe93 <dngthe93@gmail.com>
2020-05-03 17:59:04 +0900

    add tmp auto-applying script
      - move ./home to ./root/home
      - add a.py

---
+++ a.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+
+import pwd
+import os
+import argparse
+import subprocess
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--uid', type=int, help='uid of the user. default: current uid')
+    args = parser.parse_args()
+
+    uid = args.uid
+    if uid is None:
+        uid = os.getuid()
+
+
+    pw = pwd.getpwuid(uid)
+    pw_dir = pw.pw_dir
+
+    dirname = './root/home'
+    filenames = os.listdir(dirname)
+    for filename in filenames:
+        fullname = os.path.join(dirname, filename)
+        if os.path.isdir(fullname):
+            command = 'cp -r %s %s/' % (fullname, pw_dir)
+        else:
+            command = 'cp %s %s/' % (fullname, pw_dir)
+
+        print(command)
+        os.system(command)
+
+
+    # tmux setting
+    p = subprocess.run(['tmux', '-V'], stdout=subprocess.PIPE)
+    tmux_ver = float(p.stdout.decode('utf-8').strip().split()[1])
+    print(tmux_ver)
+
+    if tmux_ver >= 2.9:
+        tmuxconf = pw_dir + '/.tmux.conf'
+        command = 'sed -i "s/window-status-current-bg red/window-status-current-style bg=red/g" ' + tmuxconf
+        print(command)
+        os.system(command)
+
+    
+
+
+if __name__ == '__main__':
+    main()
